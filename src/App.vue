cfd68cf9b76b94f2a4f01b4f3f93103b8b5b8746
cristian <diazmolina1@gmail.com>
2019-06-26 19:37:50 -0500

    add all

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   <Api/>
+  </div>
+</template>
+
+<script>
+
+import Api from './components/Api.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Api
+  }
+}
+</script>
---
+++ src/components/Api.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="item in todos" :key="item.id">
+          <td >{{item.id}}</td>
+          <td >{{item.title}}</td>
+          <td>{{item.userId}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "api",
+  data() {
+    return {
+      todos: null
+    };
+  },
+  mounted(){
+      this.getTodos();
+  },
+  methods:{
+      getTodos(){
+          axios.get('https://jsonplaceholder.typicode.com/todos').then(
+              response =>{
+                  this.todos = response.data
+              }
+          ).catch(e=>console.log(e))
+      }
+  }
+};
+</script>
+
+<style>
+</style>
